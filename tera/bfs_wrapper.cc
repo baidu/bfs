291d765e0a20d2df67324461c1f04ff7efa1ba6b
bluebore <shiguang31@163.com>
2015-05-07 14:31:50 +0800

    Add bfs wrapper for tera

---
+++ tera/bfs_wrapper.cc
@@ -0,0 +1,120 @@
+// Copyright (c) 2015, Baidu.com, Inc. All Rights Reserved
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Author: yanshiguang02@baidu.com
+
+#include "bfs_wrapper.h"
+
+#include <bfs.h>
+
+namespace bfs {
+
+int32_t BfsFile::Write(const char* buf, int32_t len) {
+    return _file->Write(buf, len);
+}
+int32_t BfsFile::Flush() {
+    return _file->Flush();
+}
+int32_t BfsFile::Sync() {
+    return _file->Sync();
+}
+int32_t BfsFile::Read(char* buf, int32_t len) {
+    return _file->Read(buf, len);
+}
+int32_t BfsFile::Pread(int64_t offset, char* buf, int32_t len) {
+    return _file->Pread(buf, len, offset);
+}
+int64_t BfsFile::Tell() {
+    return _file->Seek(0, SEEK_CUR);
+}
+int32_t BfsFile::Seek(int64_t offset) {
+    return _file->Seek(offset, SEEK_SET);
+}
+int32_t BfsFile::CloseFile() {
+    return _file->Close();
+}
+
+
+BfsImpl::BfsImpl(const std::string& conf) {
+    if (!FS::OpenFileSystem(conf.c_str(), &_fs)) {
+        assert(0);
+    }
+}
+
+int32_t BfsImpl::CreateDirectory(const std::string& path) {
+    if (!_fs->CreateDirectory(path.c_str())) {
+        return -1;
+    }
+    return 0;
+}
+int32_t BfsImpl::DeleteDirectory(const std::string& path) {
+    if (!_fs->DeleteDirectory(path.c_str(), true)) {
+        return -1;
+    }
+    return 0;
+}
+int32_t BfsImpl::Exists(const std::string& filename) {
+    if (!_fs->Access(filename.c_str(), 0)) {
+        return -1;
+    }
+    return 0;
+}
+int32_t BfsImpl::Delete(const std::string& filename) {
+    if (!_fs->DeleteFile(filename.c_str())) {
+        return -1;
+    }
+    return 0;
+}
+int32_t BfsImpl::GetFileSize(const std::string& filename, uint64_t* size) {
+    BfsFileInfo fileinfo;
+    if (!_fs->Stat(filename.c_str(), &fileinfo)) {
+        return -1;
+    }
+    *size = fileinfo.size;
+    return 0;
+}
+int32_t BfsImpl::Rename(const std::string& from, const std::string& to) {
+    if (!_fs->Rename(from.c_str(), to.c_str())) {
+        return -1;
+    }
+    return 0;
+}
+int32_t BfsImpl::Copy(const std::string& from, const std::string& to) {
+    return 0;
+}
+
+int32_t BfsImpl::ListDirectory(const std::string& path, std::vector<std::string>* result) {
+    if (result == NULL) {
+        return -1;
+    }
+    bfs::BfsFileInfo* files = NULL;
+    int num = 0;
+    if (!_fs->ListDirectory(path.c_str(), &files, &num)) {
+        return -1;
+    }
+    for (int i = 0; i < num; i++) {
+        result->push_back(files[i].name);
+    }
+    delete files;
+    return 0;
+}
+
+leveldb::DfsFile* BfsImpl::OpenFile(const std::string& filename, int32_t flags) {
+    File* file = NULL;
+    if (!_fs->OpenFile(filename.c_str(), flags, &file)) {
+        return NULL;
+    }
+    return NULL;
+}
+
+} // namespace
+
+extern "C" {
+
+leveldb::Dfs* NewDfs(const char* conf) {
+    return new bfs::BfsImpl(conf);
+}
+
+}
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
---
+++ tera/dfs.h
@@ -0,0 +1,97 @@
+// Copyright (c) 2015, Baidu.com, Inc. All Rights Reserved
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef  TERA_LEVELDB_DFS_H_
+#define  TERA_LEVELDB_DFS_H_
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+namespace leveldb {
+
+class DfsFile {
+public:
+    DfsFile() {}
+    virtual ~DfsFile() {}
+    /// Returns the number of bytes written, -1 on error.
+    virtual int32_t Write(const char* buf, int32_t len) = 0;
+    /// Returns 0 on success.
+    virtual int32_t Flush() = 0;
+    /// Returns 0 on success.
+    virtual int32_t Sync() = 0;
+    /// Returns the number of bytes actually read, possibly less
+    /// than than length;-1 on error.
+    virtual int32_t Read(char* buf, int32_t len) = 0;
+    /// Returns the number of bytes actually read, possibly less than
+    /// than length;-1 on error.
+    virtual int32_t Pread(int64_t offset, char* buf, int32_t len) = 0;
+    /// Return Current offset.
+    virtual int64_t Tell() = 0;
+    /// Returns 0 on success.
+    virtual int32_t Seek(int64_t offset) = 0;
+    /// Returns 0 on success.
+    virtual int32_t CloseFile() = 0;
+private:
+    DfsFile(const DfsFile&);
+    void operator=(const DfsFile&);
+};
+
+enum open_type {
+  RDONLY = 1,
+  WRONLY = 2
+};
+
+class Dfs {
+public:
+    Dfs() {}
+    virtual ~Dfs() {}
+    /// Returns 0 on success.
+    virtual int32_t CreateDirectory(const std::string& path) = 0;
+    /// Returns 0 on success.
+    virtual int32_t DeleteDirectory(const std::string& path) = 0;
+    /// Returns 0 on success.
+    virtual int32_t Exists(const std::string& filename) = 0;
+    /// Returns 0 on success.
+    virtual int32_t Delete(const std::string& filename) = 0;
+    /// Returns 0 on success.
+    virtual int32_t GetFileSize(const std::string& filename, uint64_t* size) = 0;
+    /// Returns 0 on success.
+    virtual int32_t Rename(const std::string& from, const std::string& to) = 0;
+    /// Returns 0 on success.
+    virtual int32_t Copy(const std::string& from, const std::string& to) = 0;
+    /// Returns 0 on success.
+    virtual int32_t ListDirectory(const std::string& path, std::vector<std::string>* result) = 0;
+    /// Returns DfsFile handler on success, NULL on error.
+    virtual DfsFile* OpenFile(const std::string& filename, int32_t flags) = 0;
+    /// Returns Dfs handler on success, NULL on error.
+    static Dfs* NewDfs(const std::string& so_path, const std::string& conf); 
+private:
+    Dfs(const Dfs&);
+    void operator=(const Dfs&);
+};
+
+/// Dfs creator type.
+typedef Dfs* (*DfsCreator)(const char*);
+
+/// Implement your own dfs like this:
+/// class MyDfsFile : public DfsFile {
+///     ...
+/// };
+/// class MyDfs : public Dfs {
+///     ...
+/// };
+///
+/// extern "C" {
+/// Dfs* NewDfs(const char* conf) {
+///     Dfs* dfs= new MyDfs();
+///     ...
+///     return dfs;
+/// }
+/// }
+
+} // namespace leveldb
+#endif  //TERA_LEVELDB_DFS_H_
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
