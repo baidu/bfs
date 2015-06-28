313a2a6e829efabed1817807c47753d978c544e2
bluebore <shiguang31@163.com>
2015-06-29 05:32:40 +0800

    Add restart_bfs.sh

---
+++ sandbox/restart_bfs.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+killall -9 chunkserver
+killall -9 nameserver
+
+cd nameserver;
+./bin/nameserver 1>nlog1 2>&1 &
+
+cd ../chunkserver1;
+./bin/chunkserver --chunkserver_port=8021 1>clog1 2>&1 &
+
+cd ../chunkserver2;
+./bin/chunkserver --chunkserver_port=8022 1>clog2 2>&1 &
+
