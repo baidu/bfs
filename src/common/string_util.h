cc1b0898f6206a6020d156e45a13a9a1dfa39cc3
bluebore <shiguang31@163.com>
2015-07-17 08:57:58 +0800

    Add string_util

---
+++ src/common/string_util.h
@@ -0,0 +1,57 @@
+// Copyright (c) 2015, Baidu.com, Inc. All Rights Reserved
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Author: yanshiguang02@baidu.com
+
+#ifndef  COMMON_STRING_UTIL_H_
+#define  COMMON_STRING_UTIL_H_
+
+namespace common {
+
+void SplitString(const std::string& full,
+                 const std::string& delim,
+                 std::vector<std::string>* result) {
+    result->clear();
+    if (full.empty()) {
+        return;
+    }
+
+    std::string tmp;
+    std::string::size_type pos_begin = full.find_first_not_of(delim);
+    std::string::size_type comma_pos = 0;
+
+    while (pos_begin != std::string::npos) {
+        comma_pos = full.find(delim, pos_begin);
+        if (comma_pos != std::string::npos) {
+            tmp = full.substr(pos_begin, comma_pos - pos_begin);
+            pos_begin = comma_pos + delim.length();
+        } else {
+            tmp = full.substr(pos_begin);
+            pos_begin = comma_pos;
+        }
+
+        if (!tmp.empty()) {
+            result->push_back(tmp);
+            tmp.clear();
+        }
+    }
+}
+
+std::string TrimString(const std::string& str, const std::string& trim) {
+    std::string::size_type pos = str.find_first_not_of(trim);
+    if (pos == std::string::npos) {
+        return "";
+    }
+    std::string::size_type pos2 = str.find_last_not_of(trim);
+    if (pos2 != std::string::npos) {
+        return str.substr(pos, pos2 - pos + 1);
+    }
+    return str.substr(pos);
+}
+
+}
+
+#endif  // COMMON_STRING_UTIL_H_
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
