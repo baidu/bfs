a7d0b0369c183f1f1aab5b79405071c363dc51f9
bluebore <shiguang31@163.com>
2015-07-17 19:45:39 +0800

    More chunkserver for test

--- sandbox/start_bfs.sh
+++ sandbox/start_bfs.sh
@@ -4,11 +4,8 @@ cd nameserver;
 ./bin/nameserver 1>nlog1 2>&1 &
 echo $! > pid
 
-cd ../chunkserver1;
-./bin/chunkserver --chunkserver_port=8021 1>clog1 2>&1 &
-echo $! > pid
-
-cd ../chunkserver2;
-./bin/chunkserver --chunkserver_port=8022 1>clog2 2>&1 &
-echo $! > pid
-
+for ((i=0;i<4;i++)) do
+    cd ../chunkserver$i;
+    ./bin/chunkserver --chunkserver_port=802$i 1>clog1 2>&1 &
+    echo $! > pid
+done
