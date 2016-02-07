ed4eaca25c7174bbf8ad1fc63930d962808fc2b6
bluebore <shiguang31@163.com>
2016-02-07 14:10:44 +0800

    Fix chunkserver restart fatal log

--- src/chunkserver/chunkserver_impl.cc
+++ src/chunkserver/chunkserver_impl.cc
@@ -142,7 +142,7 @@ void ChunkServerImpl::Register() {
         return;
     }
     if (response.status() != kOK) {
-        LOG(FATAL, "Register return %s", StatusCode_Name(response.status()).c_str());
+        LOG(WARNING, "Register return %s", StatusCode_Name(response.status()).c_str());
         work_thread_pool_->DelayTask(5000, boost::bind(&ChunkServerImpl::Register, this));
         return;
     }
