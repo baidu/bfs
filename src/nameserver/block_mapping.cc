4bda4896cbd27b6669c00e44066fd137f180d700
yvxiang <linyvxiang@gmail.com>
2016-03-01 11:36:44 +0800

    Use SetState

--- src/nameserver/block_mapping.cc
+++ src/nameserver/block_mapping.cc
@@ -25,8 +25,9 @@ NSBlock::NSBlock()
 }
 NSBlock::NSBlock(int64_t block_id, int32_t replica,
                  int64_t block_version, int64_t block_size)
-    : id(block_id), version(block_version), block_size(block_size),
-      expect_replica_num(replica), recover_stat(kNotInRecover) {
+    : id(block_id), version(block_version),
+      block_size(block_size), expect_replica_num(replica),
+      recover_stat(block_version < 0 ? kBlockWriting : kNotInRecover) {
 }
 
 BlockMapping::BlockMapping() : next_block_id_(1), safe_mode_(true) {}
@@ -89,9 +90,6 @@ void BlockMapping::AddNewBlock(int64_t block_id, int32_t replica,
                                const std::vector<int32_t>* init_replicas) {
     NSBlock* nsblock = NULL;
     nsblock = new NSBlock(block_id, replica, version, size);
-    if (version < 0) {
-        nsblock->recover_stat = kBlockWriting;
-    }
     if (init_replicas) {
         if (nsblock->recover_stat == kNotInRecover) {
             nsblock->replica.insert(init_replicas->begin(), init_replicas->end());
@@ -509,7 +507,7 @@ void BlockMapping::DealWithDeadBlock(int32_t cs_id, int64_t block_id) {
         } else if (block->recover_stat == kCheck) {
             LOG(INFO, "Recovering replica dead #%ld C%d R%lu IR%lu retry recover",
                 block_id, cs_id, replica.size(), inc_replica.size());
-            block->recover_stat = kNotInRecover;
+            SetState(block, kNotInRecover);
             //RemoveFromRecoverCheckList(cs_id, block_id);
         } // else kBlockWriting
     } else {
@@ -767,7 +765,7 @@ void BlockMapping::PickRecoverFromSet(int32_t cs_id, int32_t quota,
         check_set->insert(block_id);
         cur_block->incomplete_replica.insert(cs_id);
         assert(cur_block->recover_stat == kHiRecover || cur_block->recover_stat == kLoRecover);
-        cur_block->recover_stat = kCheck;
+        SetState(cur_block, kCheck);
         LOG(INFO, "PickRecoverBlocks for C%d #%ld source: C%d ", cs_id, block_id, src_id);
         thread_pool_.DelayTask(FLAGS_recover_timeout * 1000,
             boost::bind(&BlockMapping::CheckRecover, this, cs_id, block_id));
@@ -786,21 +784,21 @@ void BlockMapping::TryRecover(NSBlock* block) {
         if (block->replica.size() == 0 && block->recover_stat != kLost) {
             LOG(INFO, "[TryRecover] lost block #%ld ", block_id);
             lost_blocks_.insert(block_id);
-            block->recover_stat = kLost;
+            SetState(block, kLost);
             lo_pri_recover_.erase(block_id);
             hi_pri_recover_.erase(block_id);
         } else if (block->replica.size() == 1 && block->recover_stat != kHiRecover) {
             hi_pri_recover_.insert(block_id);
             LOG(INFO, "[TryRecover] need more recover: #%ld %s->kHiRecover",
                 block_id, RecoverStat_Name(block->recover_stat).c_str());
-            block->recover_stat = kHiRecover;
+            SetState(block, kHiRecover);
             lost_blocks_.erase(block_id);
             lo_pri_recover_.erase(block_id);
         } else if (block->replica.size() > 1 && block->recover_stat != kLoRecover) {
             lo_pri_recover_.insert(block_id);
             LOG(INFO, "[TryRecover] need more recover: #%ld %s->kLoRecover",
                 block_id, RecoverStat_Name(block->recover_stat).c_str());
-            block->recover_stat = kLoRecover;
+            SetState(block, kLoRecover);
             lost_blocks_.erase(block_id);
             hi_pri_recover_.erase(block_id);
         } // else  Don't change recover_stat
@@ -838,6 +836,7 @@ void BlockMapping::CheckRecover(int32_t cs_id, int64_t block_id) {
 }
 
 void BlockMapping::InsertToIncomplete(int64_t block_id, const std::set<int32_t>& inc_replica) {
+    mu_.AssertHeld();
     std::set<int32_t>::const_iterator cs_it = inc_replica.begin();
     for (; cs_it != inc_replica.end(); ++cs_it) {
         incomplete_[*cs_it].insert(block_id);
