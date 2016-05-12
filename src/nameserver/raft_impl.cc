96dd1f678494a71c8660a38811dd3289a2b51c5e
bluebore <shiguang31@163.com>
2016-05-12 17:17:33 +0800

    Add raft ha_strategy

---
+++ src/nameserver/raft_impl.cc
@@ -0,0 +1,50 @@
+// Copyright (c) 2016, Baidu.com, Inc. All Rights Reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Author: yanshiguang02@baidu.com
+
+#include "raft_impl.h"
+#include "raft_node.h"
+
+#include <gflags/gflags.h>
+
+DECLARE_string(nameserver_nodes);
+DECLARE_int32(raft_node_index);
+DECLARE_string(raftdb_path);
+
+namespace baidu {
+namespace bfs {
+
+RaftImpl::RaftImpl() {
+    raft_node_ = new RaftNodeImpl(FLAGS_nameserver_nodes, FLAGS_raft_node_index, FLAGS_raftdb_path);
+}
+
+RaftImpl::~RaftImpl() {
+    delete raft_node_;
+}
+
+bool RaftImpl::IsLeader(std::string* leader_addr) {
+    std::string leader;
+    bool ret = raft_node_->GetLeader(&leader);
+    if (leader_addr && ret) {
+        *leader_addr = leader;
+    }
+    return ret;
+}
+
+bool RaftImpl::Log(const std::string& entry, int timeout_ms) {
+    return raft_node_->AppendLog(entry, timeout_ms);
+}
+
+void RaftImpl::RegisterCallback(boost::function<void (const std::string& log)> callback) {
+    return raft_node_->RegisterCallback(callback);
+}
+
+google::protobuf::Service* RaftImpl::GetService() {
+    return raft_node_;
+}
+}
+}
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
