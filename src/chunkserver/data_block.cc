854211bc6db36990b19db13ecb1d31a616b52b5c
yvxiang <yangce@baidu.com>
2016-05-20 19:11:43 +0800

    Add assert

--- src/chunkserver/data_block.cc
+++ src/chunkserver/data_block.cc
@@ -368,6 +368,7 @@ void Block::DiskWrite() {
                 file_desc_ = -1;
                 g_writing_blocks.Dec();
             }
+            assert(recv_window_);
             if (recv_window_->Size()) {
                 LOG(INFO, "#%ld recv_window fragments: %d\n",
                         meta_.block_id, recv_window_->Size());
