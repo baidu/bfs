efd520b38f5f52e223a1d797e6b705135855af22
yvxiang <yangce@baidu.com>
2016-05-30 13:36:00 +0800

    Fix headfile

--- src/nameserver/block_mapping_manager.h
+++ src/nameserver/block_mapping_manager.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BLOCK_MAPPING_MANAGER_H_
-#define BLOCK_MAPPING_MANAGER_H_
+#ifndef BFS_BLOCK_MAPPING_MANAGER_H_
+#define BFS_BLOCK_MAPPING_MANAGER_H_
 
 #include <stdint.h>
 
