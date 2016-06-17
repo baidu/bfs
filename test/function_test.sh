e47e0d825b81a4d3fbd165b7855043e46fa7f2ed
myawan <185829549@qq.com>
2016-06-17 17:35:34 +0800

    update test/function_test.sh

--- test/function_test.sh
+++ test/function_test.sh
@@ -2,7 +2,7 @@
 
 NOSE_PATH=../thirdsrc/
 
-if [ -d ./nose ]
+if [ ! -d ./nose ]
 then
     cp ${NOSE_PATH}/nose.tar.gz ./
     tar -xzvf nose.tar.gz
@@ -10,31 +10,33 @@ fi
 
 cp ../mark ./
 cp ../sandbox/*.sh ./
+mkdir -p ./log
 
-STATUS=0
+sh -x prepare_data.sh &>./log/prepare_data.log
 
 # test start and stop nameserver
-nosetests test_nameserver_start_stop.py -s
+nosetests test_nameserver_start_stop.py -s -v -x &>./log/test_nameserver_start_stop.log 
 if [ $? -ne 0 ]
 then
     echo "test_nameserver_start_stop.py case FAIL, exit"
-    STATUS=1
+    exit 1 
 fi
 
 # use mark test read and write
-nosetests test_put_read.py -s
+nosetests test_put_read.py -s -v -x &>./log/test_put_read.log
 if [ $? -ne 0 ]
 then
     echo "test_put_read.py case FAIL, exit"
-    STATUS=1
+    exit 1
 fi
 
 # use bfs_client test file operate
-nosetests test_file_operate.py -s
+nosetests test_file_operate.py -s -v -x &>./log/test_file_operate.log
 if [ $? -ne 0 ]
 then
     echo "test_file_operate.py case FAIL, exit"
-    STATUS=1
+    exit 1
 fi
 
-exit ${STATUS}
+exit 0
+
