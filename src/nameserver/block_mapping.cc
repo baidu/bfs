a87284053842d67656447765b7571ef01b657fe0
bluebore <shiguang31@163.com>
2016-08-30 15:49:13 +0800

    Adjust the log level

--- src/nameserver/block_mapping.cc
+++ src/nameserver/block_mapping.cc
@@ -160,7 +160,7 @@ bool BlockMapping::UpdateWritingBlock(NSBlock* nsblock,
                 SetState(nsblock, kNotInRecover);
                 TryRecover(nsblock);
                 if (replica.size() >= 2) {
-                    LOG(DEBUG, "Drop replica #%ld C%d V%ld R%lu IR%lu",
+                    LOG(INFO, "Drop replica #%ld C%d V%ld R%lu IR%lu",
                         block_id, cs_id, block_version ,
                         replica.size(), inc_replica.size());
                     return false;
@@ -318,7 +318,7 @@ bool BlockMapping::UpdateIncompleteBlock(NSBlock* nsblock,
                 SetState(nsblock, kNotInRecover);
                 TryRecover(nsblock);
                 if (replica.size() >= 2) {
-                    LOG(DEBUG, "Drop replica #%ld C%d V%ld R%lu IR%lu",
+                    LOG(INFO, "Drop replica #%ld C%d V%ld R%lu IR%lu",
                         block_id, cs_id, block_version ,
                         replica.size(), inc_replica.size());
                     return false;
