15801eb4a1b68bdd351a790f8de879457f007b78
yvxiang <yangce@baidu.com>
2016-08-31 15:22:58 +0800

    Fix log level

--- src/chunkserver/chunkserver_impl.cc
+++ src/chunkserver/chunkserver_impl.cc
@@ -257,7 +257,7 @@ void ChunkServerImpl::SendBlockReport() {
     bool ret = nameserver_->SendRequest(&NameServer_Stub::BlockReport, &request, &response, 600);
     int64_t after_report = common::timer::get_micros();
     if (after_report - before_report > 20 * 1000 * 1000) {
-        LOG(INFO, "Block report use %lf ms", (after_report - before_report) / 1000);
+        LOG(WARNING, "Block report use %ld ms", (after_report - before_report) / 1000);
     }
     if (!ret) {
         LOG(WARNING, "Block reprot fail\n");
