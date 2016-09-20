d0a921dbf902f490615ada2178f3c57a481ba4f3
yvxiang <yangce@baidu.com>
2016-09-20 09:33:48 +0800

    Fix

--- src/sdk/file_impl.cc
+++ src/sdk/file_impl.cc
@@ -325,7 +325,7 @@ int32_t FileImpl::AddBlock() {
     for (int i = 0; i < cs_size; i++) {
         const std::string& addr = block_for_write_->chains(i).address();
         rpc_client_->GetStub(addr, &chunkservers_[addr]);
-        write_windows_[addr] = new common::SlidingWindow<int>(20,
+        write_windows_[addr] = new common::SlidingWindow<int>(10,
                                boost::bind(&FileImpl::OnWriteCommit, this, _1, _2));
         cs_errors_[addr] = false;
         WriteBlockRequest create_request;
@@ -411,10 +411,10 @@ int32_t FileImpl::Write(const char* buf, int32_t len) {
     while (w < len) {
         MutexLock lock(&mu_, "WriteInternal", 1000);
         if (write_buf_ == NULL) {
-            write_buf_ = new WriteBuffer(++last_seq_, 64 * 1024,
+            write_buf_ = new WriteBuffer(++last_seq_, 256 * 1024,
                                          block_for_write_->block_id(),
                                          block_for_write_->block_size());
-            g_writing_buffer_size.Add(64 * 1024);
+            g_writing_buffer_size.Add(256 * 1024);
         }
         if ( (len - w) < write_buf_->Available()) {
             write_buf_->Append(buf+w, len-w);
@@ -694,7 +694,7 @@ int32_t FileImpl::Sync() {
 }
 
 int32_t FileImpl::Close() {
-    common::timer::AutoTimer at(500, "Close", name_.c_str());
+    common::timer::AutoTimer at(50, "Close", name_.c_str());
     MutexLock lock(&mu_, "Close", 1000);
     bool need_report_finish = false;
     int64_t block_id = -1;
@@ -708,6 +708,7 @@ int32_t FileImpl::Close() {
         }
         write_buf_->SetLast();
         StartWrite();
+        LOG(INFO, "WRITE QUEUE SIZE: %d", write_queue_.size());
 
         //common::timer::AutoTimer at(1, "LastWrite", _name.c_str());
         int wait_time = 0;
