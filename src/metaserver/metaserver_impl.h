21d657e1254d0996c17fc00a86c4127e679bdee2
bluebore <shiguang31@163.com>
2016-10-25 21:31:59 +0800

    Ms update

---
+++ src/metaserver/metaserver_impl.h
@@ -0,0 +1,107 @@
+// Copyright (c) 2014, Baidu.com, Inc. All Rights Reserved
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Author: yanshiguang02@baidu.com
+
+#ifndef  BFS_NAMESERVER_IMPL_H_
+#define  BFS_NAMESERVER_IMPL_H_
+
+#include <common/thread_pool.h>
+
+#include "proto/nameserver.pb.h"
+
+namespace sofa {
+namespace pbrpc {
+class HTTPRequest;
+class HTTPResponse;
+}
+}
+
+namespace baidu {
+namespace bfs {
+
+class BlockMappingManager;
+
+enum RecoverMode {
+    kStopRecover = 0,
+    kHiOnly = 1,
+    kRecoverAll = 2,
+};
+
+enum DisplayMode {
+    kDisplayAll = 0,
+    kAliveOnly = 1,
+    kDeadOnly = 2,
+    kOverload = 3,
+};
+
+class MetaServerImpl : public MetaServer {
+public:
+    MetaServerImpl(Sync* sync);
+    virtual ~MetaServerImpl();
+    void AddBlock(::google::protobuf::RpcController* controller,
+                       const AddBlockRequest* request,
+                       AddBlockResponse* response,
+                       ::google::protobuf::Closure* done);
+    void SyncBlock(::google::protobuf::RpcController* controller,
+                       const SyncBlockRequest* request,
+                       SyncBlockResponse* response,
+                       ::google::protobuf::Closure* done);
+    void FinishBlock(::google::protobuf::RpcController* controller,
+                       const FinishBlockRequest* request,
+                       FinishBlockResponse* response,
+                       ::google::protobuf::Closure* done);
+    void Register(::google::protobuf::RpcController* controller,
+                       const ::baidu::bfs::RegisterRequest* request,
+                       ::baidu::bfs::RegisterResponse* response,
+                       ::google::protobuf::Closure* done);
+    void BlockReport(::google::protobuf::RpcController* controller,
+                       const BlockReportRequest* request,
+                       BlockReportResponse* response,
+                       ::google::protobuf::Closure* done);
+    void BlockReceived(::google::protobuf::RpcController* controller,
+                       const BlockReceivedRequest* request,
+                       BlockReceivedResponse* response,
+                       ::google::protobuf::Closure* done);
+    void PushBlockReport(::google::protobuf::RpcController* controller,
+                       const PushBlockReportRequest* request,
+                       PushBlockReportResponse* response,
+                       ::google::protobuf::Closure* done);
+
+private:
+    void Register();
+    void CheckRecoverMode();
+    void LeaveReadOnly();
+    void ListRecover(sofa::pbrpc::HTTPResponse* response);
+    void TransToString(const std::map<int32_t, std::set<int64_t> >& chk_set,
+                       std::string* output);
+    void TransToString(const std::set<int64_t>& block_set, std::string* output);
+    bool CheckFileHasBlock(const FileInfo& file_info,
+                           const std::string& file_name,
+                           int64_t block_id);
+private:
+    /// Global thread pool
+    ThreadPool* read_thread_pool_;
+    ThreadPool* work_thread_pool_;
+    ThreadPool* report_thread_pool_;
+    ThreadPool* heartbeat_thread_pool_;
+    /// ChunkServer map
+    ChunkServerManager* chunkserver_manager_;
+    /// Block map
+    BlockMappingManager* block_mapping_manager_;
+
+    volatile bool readonly_;
+    volatile int recover_timeout_;
+    RecoverMode recover_mode_;
+    int64_t start_time_;
+    /// Namespace
+    NameSpace* namespace_;
+};
+
+} // namespace bfs
+} // namespace biadu
+
+#endif  //BFS_NAMESERVER_IMPL_H_
+
+/* vim: set expandtab ts=4 sw=4 sts=4 tw=100: */
