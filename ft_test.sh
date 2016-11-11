6ebf35285ca9ee71ac09540c94753f856f8f96fb
wangwenming <wangwenming@baidu.com>
2016-11-11 14:40:38 +0800

    add some header for shell script

--- ft_test.sh
+++ ft_test.sh
@@ -1,4 +1,11 @@
-set -x -e
+#!/usr/bin/env bash
+set -x
+set -o pipefail
+export PS4='+{$LINENO `date "+%Y-%m-%d_%H:%M:%S"` :${FUNCNAME[0]}}    '
+cur=`dirname "${0}"`
+cd "${cur}"
+cur=`pwd`
+set -e
 
 make install;
 make test; cd sandbox; sh clear.sh; sh deploy.sh; sh start_bfs.sh;
