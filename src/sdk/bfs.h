d3e96787ad0d8d413e0694a33e39b85ef25052dc
yvxiang <yangce@baidu.com>
2016-12-12 14:09:10 +0800

    Use const var instead of macro
    (#684)

--- src/sdk/bfs.h
+++ src/sdk/bfs.h
@@ -16,16 +16,16 @@
 namespace baidu {
 namespace bfs {
 
-#define OK 0
-#define BAD_PARAMETER -1
-#define PERMISSION_DENIED -2
-#define NOT_ENOUGH_QUOTA -3
-#define NETWORK_UNAVAILABLE -4
-#define TIMEOUT -5
-#define NOT_ENOUGH_SPACE -6
-#define OVERLOAD -7
-#define META_NOT_AVAILABLE -8
-#define UNKNOWN_ERROR -9
+const int OK = 0;
+const int BAD_PARAMETER = -1;
+const int PERMISSION_DENIED = -2;
+const int NOT_ENOUGH_QUOTA = -3;
+const int NETWORK_UNAVAILABLE = -4;
+const int TIMEOUT = -5;
+const int NOT_ENOUGH_SPACE = -6;
+const int OVERLOAD = -7;
+const int META_NOT_AVAILABLE = -8;
+const int UNKNOWN_ERROR = -9;
 
 const char* StrError(int error_code);
 
