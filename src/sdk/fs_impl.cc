4af2080e9fd2fd77c2b42d08d6af79c78934c7d5
yvxiang <yangce@baidu.com>
2016-12-14 15:01:55 +0800

    Fix typo for write mode flag
    (#619)

--- src/sdk/fs_impl.cc
+++ src/sdk/fs_impl.cc
@@ -23,7 +23,7 @@
 
 DECLARE_int32(sdk_thread_num);
 DECLARE_string(nameserver_nodes);
-DECLARE_string(sdk_wirte_mode);
+DECLARE_string(sdk_write_mode);
 
 namespace baidu {
 namespace bfs {
@@ -306,7 +306,7 @@ int32_t FSImpl::OpenFile(const char* path, int32_t flags, int32_t mode,
     }
     WriteOptions write_option = options;
     if (options.write_mode == kWriteDefault) {
-        if (FLAGS_sdk_wirte_mode == "fanout") {
+        if (FLAGS_sdk_write_mode == "fanout") {
             write_option.write_mode = kWriteFanout;
         } else {
             write_option.write_mode = kWriteChains;
