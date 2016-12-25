2b3537c0933ada8bd549f3b14053364350f531a1
yvxiang <yangce@baidu.com>
2016-12-25 10:32:55 +0800

    Fix log level
    (#661)

--- src/nameserver/namespace.cc
+++ src/nameserver/namespace.cc
@@ -39,7 +39,7 @@ NameSpace::NameSpace(bool standalone): version_(0), last_entry_id_(1),
     leveldb::Status s = leveldb::DB::Open(options, FLAGS_namedb_path, &db_);
     if (!s.ok()) {
         db_ = NULL;
-        LOG(WARNING, "Open leveldb fail: %s", s.ToString().c_str());
+        LOG(ERROR, "Open leveldb fail: %s", s.ToString().c_str());
         exit(EXIT_FAILURE);
     }
     if (standalone) {
