0df78f50124dfee383ea90b671c6d86d9d7d6a19
Zhu Yuejun <jayzhu@bupt.edu.cn>
2017-02-17 00:43:48 +0800

    Update file_impl_wrapper.h

--- src/sdk/file_impl_wrapper.h
+++ src/sdk/file_impl_wrapper.h
@@ -18,7 +18,7 @@ class FileImpl;
 class FSImpl;
 class RpcClient;
 
-class FileImplWrapper : public File, public baidu::bfs::noncopyable {
+class FileImplWrapper : public File, public noncopyable {
 public:
     FileImplWrapper(FSImpl* fs, RpcClient* rpc_client,
             const std::string& name, int32_t flags, const WriteOptions& options);
