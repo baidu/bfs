d64bec81cd620b04aa25f1b65ffbd6216e190e0a
yangce <yangce@baidu.com>
2017-02-27 18:57:09 +0800

    Fix copyright
    (#622)

--- src/nameserver/file_lock_manager.cc
+++ src/nameserver/file_lock_manager.cc
@@ -1,4 +1,4 @@
-/* // Copyright (c) 2016, Baidu.com, Inc. All Rights Reserved. */
+// Copyright (c) 2016, Baidu.com, Inc. All Rights Reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
