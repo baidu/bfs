d2492848884d268062692449013f44b7f2b22e31
yangce <yangce@baidu.com>
2017-02-28 14:10:04 +0800

    Remote debug log
    (#622)

--- src/nameserver/file_lock.cc
+++ src/nameserver/file_lock.cc
@@ -14,15 +14,12 @@ FileLockManager* WriteLock::file_lock_manager_ = NULL;
 FileLockManager* ReadLock::file_lock_manager_ = NULL;
 
 WriteLock::WriteLock(const std::string& file_path) {
-    LOG(DEBUG, "Write lock guard for %s", file_path.c_str());
     file_path_.push_back(file_path);
     file_lock_manager_->WriteLock(file_path);
 }
 
 WriteLock::WriteLock(const std::string& file_path_a,
                                  const std::string& file_path_b) {
-    LOG(DEBUG, "Write lock guard for %s and %s",
-                 file_path_a.c_str(), file_path_b.c_str());
     int r = strcmp(file_path_a.c_str(),file_path_b.c_str());
     if (r == 0) {
         file_path_.push_back(file_path_a);
@@ -42,24 +39,19 @@ WriteLock::WriteLock(const std::string& file_path_a,
 
 WriteLock::~WriteLock() {
     if (file_path_.size() == 1) {
-        LOG(DEBUG, "Release write lock guard for %s", file_path_[0].c_str());
         file_lock_manager_->Unlock(file_path_[0]);
     } else {
-        LOG(DEBUG, "Release write lock guard for %s and %s",
-                      file_path_[1].c_str(), file_path_[0].c_str());
         file_lock_manager_->Unlock(file_path_[1]);
         file_lock_manager_->Unlock(file_path_[0]);
     }
 }
 
 ReadLock::ReadLock(const std::string& file_path) {
-    LOG(DEBUG, "Read lock guard for %s", file_path.c_str());
     file_path_ = file_path;
     file_lock_manager_->ReadLock(file_path);
 }
 
 ReadLock::~ReadLock() {
-    LOG(DEBUG, "Release read lock guard for %s", file_path_.c_str());
     file_lock_manager_->Unlock(file_path_);
 }
 
