4e7977323dfdd0cdd19f72e42735ff0602d6c5a9
apady <frankchenls@outlook.com>
2018-11-29 15:04:46 +0800

    Update bfs_c.cc

--- src/sdk/bfs_c.cc
+++ src/sdk/bfs_c.cc
@@ -340,7 +340,7 @@ int bfs_du(bfs_fs_t* fs, const char* path) {
 int bfs_rm_dir(bfs_fs_t* fs, const char* path, bool recursive) {
     return fs->bfs_fs->DeleteDirectory(path, recursive);
 }
-//bug 修复
+    
 int bfs_change_replica_num(bfs_fs_t* fs, const char* path,
         const char* bfs_fslica_num) {
     if (!isdigit(*bfs_fslica_num)) {
